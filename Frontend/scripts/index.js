6c93dac0dd7e86439ae0645c2ddd7efbab13da6d
Anandhu P A <anandhu@webcastle.in>
2025-02-04 15:35:42 +0530

    initial setup

---
+++ Frontend/scripts/index.js
@@ -0,0 +1,8 @@
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the div
+    let myDiv = document.getElementById("myDiv");
+    
+    // Change the text inside the div
+    myDiv.innerText = "Text changed by JavaScript!";
+});
